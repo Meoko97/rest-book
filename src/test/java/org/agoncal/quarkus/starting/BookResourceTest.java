af19113d91291a7fc5a5f51ea33c03178612ae5b
Daniel Garcia <dgarmar97@gmail.com>
2023-10-28 12:16:57 +0200

    Commit-3: add unit test to BookResource

--- src/test/java/org/agoncal/quarkus/starting/BookResourceTest.java
+++ src/test/java/org/agoncal/quarkus/starting/BookResourceTest.java
@@ -1,21 +1,53 @@
 package org.agoncal.quarkus.starting;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.HttpHeaders.ACCEPT;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.MediaType.TEXT_PLAIN;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class BookResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void shouldGetAllBooks() {
         given()
-          .when().get("/api/books")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+            .header(ACCEPT, APPLICATION_JSON)
+        .when()
+            .get("/api/books")
+        .then()
+            .statusCode(HttpStatus.SC_OK)
+            .body("size()", is(4));
     }
 
+    @Test
+    public void shouldGetCountAllBooks() {
+        given()
+            .header(ACCEPT, TEXT_PLAIN)
+        .when()
+            .get("/api/books/count")
+        .then()
+            .statusCode(HttpStatus.SC_OK)
+            .body( is("4"));
+    }
+
+    @Test
+    public void shouldGetBook() {
+        given()
+            .header(ACCEPT, APPLICATION_JSON)
+                .pathParam("id", 1)
+        .when()
+            .get("/api/books/{id}")
+        .then()
+            .statusCode(HttpStatus.SC_OK)
+            .body("title", is("Understanding Quarkus"))
+            .body("author", is("Antonio"))
+            .body("genre", is("IT"))
+            .body("id", is(1))
+            .body("yearOfPublication", is(2020));
+    }
 }
