cc291c8ea9c6ec017e3dd967fe5a498d855b9f42
Daniel Garcia <dgarmar97@gmail.com>
2023-10-28 11:33:34 +0200

    Commit-1: add inject repository

--- src/main/java/org/agoncal/quarkus/starting/BookResource.java
+++ src/main/java/org/agoncal/quarkus/starting/BookResource.java
@@ -1,34 +1,45 @@
 package org.agoncal.quarkus.starting;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.Optional;
+
 import org.agoncal.quarkus.starting.model.Book;
+import org.agoncal.quarkus.starting.service.BookRepository;
+import org.jboss.logging.Logger;
 
 @Path("/api/books")
 @Produces(MediaType.APPLICATION_JSON)
 public class BookResource {
 
-    private List<Book> listOfBooks = List.of(
-        new Book(1, "Understanding Quarkus", "Antonio", 2020, "IT"),
-        new Book(2, "Practising Quarkus", "Antonio", 2020, "IT"),
-        new Book(3, "Effective Java", "Josh Bloch", 2001, "IT"),
-        new Book(4, "Thinking in Java", "Bruce Eckel", 1998, "IT")
-    );
+    @Inject
+    BookRepository repository;
+    @Inject
+    Logger logger;
 
     @GET
     public List<Book> getAllBooks() {
-        return listOfBooks;
+        logger.info("Return all the books saved");
+        return repository.getAllBooks();
     }
 
     @GET
     @Path("/count")
     @Produces(MediaType.TEXT_PLAIN)
     public int countAllBooks() {
-        return listOfBooks.size();
+        logger.info("Returns the number of books saved");
+        return repository.getAllBooks().size();
     }
 
-
+    @GET
+    @Path("/{id}")
+    public Optional<Book> getBook(@PathParam("id") int id) {
+        logger.info("Returns a single book with id " + id);
+        return repository.getBook(id);
+    }
 }
